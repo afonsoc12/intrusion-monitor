b9d3cb3e7ad856c253ac1fa4d786eb3e894b1984
Afonso Costa <afonsoc1997@gmail.com>
2021-03-28 16:47:45 +0100

    setup.py configured Installed versioneer

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import versioneer
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="intrusion-monitor",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Afonso Costa",
+    description="Your single solution for export",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/afonsoc12/intrusion-monitor",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests>=2.25.1,<3',
+        'pygeohash>=1.2.0,<2',
+        'influxdb>=5.3.1,<6'
+    ],
+    entry_points={
+        'console_scripts': [
+            'intrusion-monitor = intrusion_monitor.__main__:main'
+        ]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
